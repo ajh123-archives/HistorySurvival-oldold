c2d5aa2f85a30a62feeeae2460104de2a5271923
ajh123 <samrosoft36@gmail.com>
2021-11-13 18:12:32 +0000

    Fix building on mac

--- include/client_main.hpp
+++ include/client_main.hpp
@@ -3,4 +3,8 @@
 
 int client_main(void);
 
+#ifndef FLT_MAX
+#define FLT_MAX 340282346638528859811704183484516925440.0f     // Maximum value of a float, from bit pattern 01111111011111111111111111111111
+#endif
+
 #endif
