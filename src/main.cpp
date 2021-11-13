55a3e06729c0f672b30762cb49bfda027342e6e6
ajh123 <ajh123@users.noreply.github.com>
2021-11-13 18:28:02 +0000

    Asio testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
 #include <main.hpp>
 #include <stdio.h>
+#include <iostream>
 #include <client_main.hpp>
 
 #ifdef _WIN32
@@ -14,8 +15,27 @@
 
 int main(void)
 {
+    asio::error_code ec;
+    asio::io_context context;
+
     //https://github.com/ajh123-development/HistorySurvival-Data/raw/main/info.json
     if (game_type == GAME_TYPE_CLIENT){
+        asio::io_service io_service;
+        asio::ip::tcp::resolver resolver(io_service);
+        asio::ip::tcp::resolver::query query("github.com", "80");
+        asio::ip::tcp::resolver::iterator iter = resolver.resolve(query);
+
+        asio::ip::tcp::endpoint endpoint = iter->endpoint();
+        asio::ip::tcp::socket socket(context);
+        socket.connect(endpoint, ec);
+
+        if (!ec) {
+            std::cout << "Connected" << std::endl;
+        }
+        else {
+            std::cout << "Failed to connect" << ec.message() << std::endl;
+        }
+
         printf("Running as client\n");
         return client_main();
     }
